11ec7f8adcd017721db0365f6689e2b58a3583e2
Carlos Maffrand <carlosmaffrand5@gmail.com>
2021-09-22 22:38:53 -0300

    commit iniclal del repo de Arquitectura de micros, contiene la solución a los ejercicios 1 y 2

---
+++ app/inc/c_func.h
@@ -0,0 +1,8 @@
+#pragma once
+
+#include <stdint.h>
+
+
+uint32_t c_sum (uint32_t firstOperand, uint32_t secondOperand);
+uint64_t c_sum64 (uint64_t firstOperand, uint64_t secondOperand);
+void zeros (uint32_t * vector, uint32_t longitud);
